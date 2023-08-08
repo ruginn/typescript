3869df30f003d8c8577f9f4893cacb35d0c3a45b
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2023-08-07 22:34:41 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,11 +9,32 @@ let users: {} = {
     user2: 'fjk'
 }
 let thisVar: any = 'this is a string'
+
+//arrays are weird, you have to say what is in the array first
 let nums: number[] = [1,2, 3, 4, 5]
 for (let num of nums){
     console.log(num)
 }
-// arrays are weird
-let myArray: [] = []
+let myArray: any[] = ['wod', 2, true]
 
 // Great for functions
+const square = (num: number) =>{
+    return num * num
+}
+
+// multiple parameters can have their type defined
+const thisFunction = (num: number, str: string, tf: boolean) => {
+
+}
+// It will complain if all the arguments are not there
+thisFunction(43, 'fd', true)
+
+// functions with default values, will go after defining
+const greet = (person: string|number = 'stranger'): string | number => {
+    console.log(person)
+    return `hello ${person}`
+}
+greet()
+greet('jeff')
+// 
+
