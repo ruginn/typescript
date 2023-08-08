b64842be4b26307a4b26ae4059a313f702105e5b
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2023-08-07 22:17:24 -0400

    playing

---
+++ index.js
@@ -0,0 +1,11 @@
+//  you must add the type for variables
+var user = 'Tron';
+var value = 1;
+var isOnline = true;
+var users = {
+    user1: 'toto',
+    user2: 'fjk'
+};
+// arrays are weird
+var myArray = [];
+// Great for functions
---
+++ index.ts
@@ -0,0 +1,19 @@
+// ** to compile, run 'tsc index.ts' in the terminal
+
+//  you must add the type for variables
+let user: string = 'Tron'
+let value: number = 1
+let isOnline: boolean = true
+let users: {} = {
+    user1: 'toto', 
+    user2: 'fjk'
+}
+let thisVar: any = 'this is a string'
+let nums: number[] = [1,2, 3, 4, 5]
+for (let num of nums){
+    console.log(num)
+}
+// arrays are weird
+let myArray: [] = []
+
+// Great for functions
